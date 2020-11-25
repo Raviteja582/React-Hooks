7637180e993bf12c8039bee8daa6b97abb3504aa
Raviteja582 <raviteja1028312@gmail.com>
2020-11-25 11:54:11 +0530

    Component Under JSX hood Understand

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
+/* After Clearing the Boiler plater code except index.js */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* After Writing the Component, to Inject the code in component to index.html
+   we need to somehow inject to dom. for that we use ReactDom which renders the jsx code 
+   return from component into html dom.
+*/
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+	return <h4>Hell World, we</h4>
+}
+
+
+/* Creating same component using JSX functions directly */
+
+const Wishing = () => {
+	return React.createElement('h1', {}, "hello world");
+}
+
+/* Another Exxample for React JSX Underhood*/
+
+function Hello() {
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+const Hi = () => {
+	return (
+		React.createElement('div', {}, React.createElement('h1', {}, 'Hi World'))
+	)
+}
+
+ReactDom.render(<Hi />, document.getElementById('root'));
